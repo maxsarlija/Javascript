385ae2d969fdbaf73cd6f4a1f4ef25a3600b0758
max.sarlija <max.sarlija@outlook.com.ar>
2022-02-24 23:31:08 -0300

    clase 6 desafio v1

---
+++ clase_6/desafio/script.js
@@ -0,0 +1,17 @@
+const productos = [
+    {marca:"Plus", precio:"120", cantidad:"0"},
+    {marca:"Plus", precio:"10", cantidad:"0"},
+    {marca:"Arcor", precio:"10", cantidad:"0"},
+    {marca:"Arcor", precio:"120", cantidad:"0"},
+    {marca:"Arcor", precio:"150", cantidad:"0"},
+    {marca:"Arcor", precio:"150", cantidad:"0"},
+    {marca:"Arcor", precio:"100", cantidad:"0"},
+    {marca:"Serenisima", precio:"100", cantidad:"0"},
+    {marca:"Serenisima", precio:"150", cantidad:"0"},
+    {marca:"Serenisima", precio:"120", cantidad:"0"},
+    {marca:"Unilever", precio:"120", cantidad:"0"},
+    {marca:"Unilever", precio:"150", cantidad:"0"},
+    {marca:"Unilever", precio:"150", cantidad:"0"},
+];    // 
+
+productos.sort();
